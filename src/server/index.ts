c7c3586e6dc755b623b6ec2410e1619daf6444ac
Kirill Kropotin <kdkropotin@gmail.com>
2025-07-13 22:39:25 +0300

    port

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,17 @@
-import express from 'express';
-import { router } from './routes';
+import express from "express";
+import { router } from "./routes";
 
 const port = Number(process.env.PORT) || 3000;
-const basename = '/hw/store';
+const basename = "/hw/store";
 
 const app = express();
 
 app.use(express.json());
-app.use(basename, express.static('dist', { index: false }));
+app.use(basename, express.static("dist", { index: false }));
 app.use(basename, router);
 
-app.listen(port, '::', () => {
-    console.log(`Example app listening at http://localhost:${port}${basename}`);
+const host = process.env.HOST || "0.0.0.0";
+
+app.listen(port, host, () => {
+  console.log(`Example app listening at http://${host}:${port}${basename}`);
 });
